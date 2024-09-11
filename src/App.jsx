9a3f7b06a057e83705029601ae683db3e0fea8f4
Ashit Rai <raiashit669@gmail.com>
2024-09-11 15:11:20 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./Components/Header";
+import Home from "./Components/Home";
+import Detail from "./Components/Detail";
+
+const App = () => {
+  return (
+    <div className="App">
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" caseSensitive={false} element={<Home/>} />
+          <Route
+            path="/detail/:id"
+            caseSensitive={false}
+            element={<Detail />}
+          />
+        </Routes>
+      </Router>
+    </div>
+  );
+};
+
+export default App;
